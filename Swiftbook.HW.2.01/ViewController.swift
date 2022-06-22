ddd4b89843be1c5da040ba9db6f01c05f7b620e5
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-06-22 16:00:53 +0300

    Initial Commit

---
+++ Swiftbook.HW.2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftbook.HW.2.01
+//
+//  Created by MacBook Air 13 on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
